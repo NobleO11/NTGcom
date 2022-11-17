4f119f3b7f5ce12b77f2260af4400d9a90ad1805
oalshamrani <oalshamrani@ntgclarity.com>
2022-11-17 12:31:49 +0300

    code

---
+++ src/main/java/com/ntg/commerce/NTGCom/NtgComApplication.java
@@ -0,0 +1,13 @@
+package com.ntg.commerce.NTGCom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NtgComApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NtgComApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ntg/commerce/NTGCom/NtgComApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntg.commerce.NTGCom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NtgComApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
